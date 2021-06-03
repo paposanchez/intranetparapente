75d3b070678a8375796102a6dbbc9d525a958cfc
paposanchez <juanpablosanchezbarcelo@gmail.com>
2021-06-03 12:18:14 -0400

    cambios en theme boostrap a tailwin

--- resources/views/servicios/index.blade.php
+++ resources/views/servicios/index.blade.php
@@ -1,4 +1,5 @@
-@extends('adminlte::page')
+@extends('layouts.parque.index')
+@section('title', 'Servicios')
 
 @section('content')
 <div class="card">
@@ -12,125 +13,77 @@
         </ul>
     </div>
 @endif
-    <div class="card-header">
-      <h3 class="card-title">Servicios</h3>
-            <div class="col col-xs-6 text-right">
-                <a class="btn btn-primary btn-sm" href="{{ url("servicios/create") }}">
-                <i class="fas fa-folder">
-                </i>
-                Crear Servicio
-                </a>
-      </div>
-      <div class="card-tools">
-        <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
-          <i class="fas fa-minus"></i></button>
-        <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
-          <i class="fas fa-times"></i></button>
-      </div>
-    </div>
-    <div class="card-body p-0">
-      <table class="table table-striped table-bordered table-list " id="departamento">
-          <thead>
-              <tr>
-                  <th>
-                      Fecha :: Hora
-                  </th>
-                  <th>
-                      Difunto
-                  </th>
-                  <th>
-                    sector
-                </th>
-                <th>
-                    parque
-                </th>
-                  <th>
-                    Acciones
-                 </th>
-              </tr>
-          </thead>
-          @foreach ($servi as $lista)
-          <tbody>
-              <tr>
-                  <td>
-                      <a>
-                        {{ $lista->fecha }} :: {{ $lista->hora }}
-                      </a>
-                  </td>
-                  <td>
-                    {{ $lista->difunto }}
-                  </td>
-                  <td>
-                    {{ $lista->sector }}
-                  </td>
-                  <td>
-                    {{ $lista->deudogestor }}
-                  </td>
-                  <td class="project-actions text-right">
-                      <a class="btn btn-info btn-sm" href="{{ route('servicios.edit', $lista->id) }}">
-                          <i class="fas fa-pencil-alt">
-                          </i>
-                          Editar
-                      </a>
-                      <a class="btn btn-danger btn-sm" href="#">
-                          <i class="fas fa-trash">
-                          </i>
-                          Borrar
-                      </a>
-                  </td>
-              </tr>
+  <table class="min-w-full table-auto">
+    <thead class="justify-between">
+      <tr class="bg-gray-800">
+        <th class="px-16 py-2">
+          <span class="text-gray-300">Fecha / Hora</span>
+        </th>
+        <th class="px-30 py-2">
+            <span class="text-gray-300">Nombre DIfunto</span>
+          </th>
+        <th class="px-16 py-2">
+          <span class="text-gray-300">Perfil</span>
+        </th>
+        <th class="px-16 py-2">
+          <span class="text-gray-300">Sector</span>
+        </th>
 
-          </tbody>
-          @endforeach
-      </table>
-    </div>
-    <!-- /.card-body -->
-  </div>
+        <th class="px-30 py-2">
+          <span class="text-gray-300">Deudo Gestor</span>
+        </th>
 
+        <th class="px-16 py-2">
+          <span class="text-gray-300">Status</span>
+        </th>
+      </tr>
+    </thead>
+    @foreach ($servi as $lista)
+    <tbody class="bg-gray-200">
+        
+      <tr class="bg-white border-4 border-gray-200">
+        <td class="px-16 py-2 flex flex-row items-center">
+            {{ $lista->fecha }} {{ $lista->hora }}
+        </td>
+        <td>
+          <span class="text-center ml-2 font-semibold">{{ $lista->difunto }}</span>
+        </td>
+        <td class="px-16 py-2">
+          <a href="#"><button class="bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-white hover:border-indigo-500 hover:text-black ">
+            Servicio
+          </button></a>
+        </td>
+        <td class="px-16 py-2">
+          <span>05/06/2020</span>
+        </td>
+        <td class="px-30 py-3">
+          <span>{{$lista->deudogestor}}</span>
+        </td>
+
+        <td class="px-16 py-2">
+          <span class="text-green-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="w-5 h5 "
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="#2c3e50"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" />
+              <path d="M5 12l5 5l10 -10" />
+            </svg>
+          </span>
+        </td>
+      </tr>
+      
+      
+      
+    </tbody>
+    @endforeach
+  </table>
+</div>
 @endsection
-@push('scripts')
-    <!-- DataTables -->
-    <script src="{{ asset('js/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('js/dataTables.bootstrap.min.js"></script>
-
-    <script>
-        $('#li_stations').addClass('active');
-        $(function () {
-            $('#stations-table').DataTable({
-            'pageLength': 25,
-            'paging'      : true,
-            'lengthChange': true,
-            'searching'   : true,
-            'ordering'    : true,
-            'info'        : false,
-            'autoWidth'   : false,
-            'order'       : [],
-            'language': {
-                "sProcessing":     "Procesando...",
-                "sLengthMenu":     "Mostrar _MENU_ registros",
-                "sZeroRecords":    "No se encontraron resultados",
-                "sEmptyTable":     "Ningún dato disponible en esta tabla",
-                "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-                "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
-                "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
-                "sInfoPostFix":    "",
-                "sSearch":         "Buscar:",
-                "sUrl":            "",
-                "sInfoThousands":  ",",
-                "sLoadingRecords": "Cargando...",
-                "oPaginate": {
-                    "sFirst":    "Primero",
-                    "sLast":     "Último",
-                    "sNext":     "Siguiente",
-                    "sPrevious": "Anterior"
-                },
-                "oAria": {
-                    "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
-                    "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-                }
-            }
-            })
-        });
-    </script>
-@endpush
 
